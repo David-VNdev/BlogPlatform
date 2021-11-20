a38ccf1753ebdafdf8dff15b78fd12c7f01dcb7f
David Bui <tbui.vdev@gmail.com>
2021-11-20 10:57:47 -0600

    MVC Design Pattern

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
 import express from "express";
 import morgan from "morgan"; //logger
-import path,{dirname} from "path";
-import { fileURLToPath } from 'url';
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import route from "./routes/index.js";
 
-
-
-import { engine,create } from "express-handlebars";
+import { engine, create } from "express-handlebars";
 const app = express();
 const port = 3000;
+
+// Middlewares
 // Static
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname,"public")));
+app.use(express.static(path.resolve(__dirname, "public")));
 // HTTP logger
 app.use(morgan("combined"));
 
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Handle bars template engine
-app.set("views", path.resolve(__dirname,"resources","views"));
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', 'hbs');
+app.set("views", path.resolve(__dirname, "resources", "views"));
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
+// Routes
+route(app);
 app.listen(3000, () => {
-	console.log(`Blog app is listening at http://localhost:${port}`);
+  console.log(`Blog app is listening at http://localhost:${port}`);
 });
