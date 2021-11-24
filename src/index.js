de4c12fa285fd2274b68bf926c3d9cc3397b0c38
David Bui <tbui.vdev@gmail.com>
2021-11-23 18:26:22 -0600

    Add update course feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import express from "express";
-import morgan from "morgan"; //logger
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import route from "./routes/index.js";
-import { engine, create } from "express-handlebars";
-import db from "./config/db/index.js";
+import express from 'express';
+import morgan from 'morgan'; //logger
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import route from './routes/index.js';
+import { engine, create } from 'express-handlebars';
+import db from './config/db/index.js';
+import methodOverride from 'method-override';
+
 const app = express();
 const port = 3000;
 
@@ -14,21 +16,31 @@ db.connect();
 // Middlewares
 // Static
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static(path.resolve(__dirname, 'public')));
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Handle bars template engine
-app.set("views", path.resolve(__dirname, "resources", "views"));
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", "hbs");
+app.set('views', path.resolve(__dirname, 'resources', 'views'));
+app.engine(
+	'.hbs',
+	engine({
+		extname: '.hbs',
+		helpers: {
+			sum: (a, b) => a + b,
+		},
+	})
+);
+app.set('view engine', 'hbs');
 
+// MEthod override
+app.use(methodOverride('_method'))
 
 // Routes
 route(app);
 app.listen(3000, () => {
-  console.log(`Node module is listening at http://localhost:${port}`);
+	console.log(`Node module is listening at http://localhost:${port}`);
 });
