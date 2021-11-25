6ab973b83ca72f176950b29929e1656a5235ae63
David Bui <tbui.vdev@gmail.com>
2021-11-25 15:53:59 -0600

    Add check-all checkbox

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
-import newsRouter from "./news.js";
-import siteRouter from "./site.js";
-import coursesRouter from "./courses.js";
-import meRouter from "./me.js";
-
+import newsRouter from './news.js';
+import siteRouter from './site.js';
+import coursesRouter from './courses.js';
+import meRouter from './me.js';
 
 function route(app) {
-	app.use("/courses", coursesRouter);
-	app.use("/news", newsRouter);
-	app.use("/me", meRouter);
+	app.use('/courses', coursesRouter);
+	app.use('/news', newsRouter);
+	app.use('/me', meRouter);
 
-	app.use("/", siteRouter);
+	app.use('/', siteRouter);
 }
 
 export default route;
