9943afcfe7f946ba96f721df6f343094842a35e1
David Bui <tbui.vdev@gmail.com>
2021-11-25 11:13:37 -0600

    Feature: Deleted Count

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,24 +1,28 @@
 import Course from '../models/Course.js';
-import {multipleMongooseToObject} from '../../util/mongoose.js';
+import { multipleMongooseToObject } from '../../util/mongoose.js';
 
 class MeController {
 	// [GET] /me/storedCourses
+
 	storedCourses(req, res, next) {
-		Course.find({})
-			.then((courses) => {
-				res.render('./me/storedCourses', {courses:multipleMongooseToObject(courses)});
-			})
-			.catch(next);
+		Promise.all([Course.countDocumentsDeleted({}), Course.find({})]).then(
+			([deletedCount, courses]) => {
+				console.log(deletedCount);
+				res.render('./me/storedCourses', {
+					courses: multipleMongooseToObject(courses),
+					deletedCount,
+				});
+			}
+		);
 	}
 	// [GET] /me/trashCourses
 
 	trashCourses(req, res, next) {
 		Course.findDeleted()
 			.then((courses) => {
-				res.render('./me/deletedCourses', {courses:multipleMongooseToObject(courses)});
+				res.render('./me/deletedCourses', { courses: multipleMongooseToObject(courses) });
 			})
 			.catch(next);
 	}
-	
 }
 export default new MeController();
